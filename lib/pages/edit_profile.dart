74c6296f69fea3009384a9552e5938272dd3ae7f
Hadziq Umar Azka Mirza <122033699+Hadziq224@users.noreply.github.com>
2024-06-15 22:36:14 +0700

    edit_profile.dart

--- lib/pages/edit_profile.dart
+++ lib/pages/edit_profile.dart
@@ -14,23 +14,35 @@ class EditProfilePage extends StatefulWidget {
 }
 
 class _EditProfilePageState extends State<EditProfilePage> {
-  final UserController userController = Get.find();
+  final UserController userController = Get.find(); // Akses data pengguna
+
+  // Kunci formulir dan pengontrol edit teks
   final _formKey = GlobalKey<FormState>();
   final _nameController = TextEditingController();
   final _emailController = TextEditingController();
+
+  // Penanganan gambar
   File? _imageFile;
   final ImagePicker _picker = ImagePicker();
   String? _selectedImageUrl;
+
+  // Instansi Firestore
   final _firestore = FirebaseFirestore.instance;
 
   @override
   void initState() {
     super.initState();
+
+    // Isi awal formulir dengan data pengguna
     _nameController.text = userController.userName.value;
     _emailController.text = userController.userEmail.value;
     _selectedImageUrl = userController.profilePictureUrl.value;
   }
 
+  /// Memperbarui informasi profil pengguna di Firestore
+  ///
+  /// Memvalidasi input formulir, memperbarui Firestore dengan data pengguna,
+  /// memperbarui status pengendali pengguna lokal, dan kembali ke halaman sebelumnya.
   Future<void> _updateProfile() async {
     if (_formKey.currentState!.validate()) {
       try {
@@ -42,33 +54,43 @@ class _EditProfilePageState extends State<EditProfilePage> {
           'email': _emailController.text,
           'profile_picture': _selectedImageUrl ?? '',
         });
+
+        // Perbarui status pengendali pengguna
         userController.userName.value = _nameController.text;
         userController.userEmail.value = _emailController.text;
         userController.setProfilePictureUrl(_selectedImageUrl ?? '');
 
         Navigator.pop(context);
       } catch (e) {
-        print("Error updating profile: $e");
-        _showDialog("Failed to update profile");
-      }
-    }
-  }
-
-  Future<void> _uploadImageToFirebase(File imageFile) async {
-    try {
-      final storageRef = firebase_storage.FirebaseStorage.instance
-          .ref()
-          .child('profile_pictures')
-          .child('${userController.userId.value}.jpg');
+        print("Error memperbarui profil: <span class="math-inline">e"\);
+\_showDialog\("Gagal memperbarui profil"\);
+\}
+\}
+\}
+/// Mengunggah gambar yang dipilih ke Firebase Storage
+///
+/// Mengunggah gambar yang disediakan ke Firebase Storage,
+/// mengambil URL unduh, dan memperbarui URL gambar yang dipilih\.
+Future<void\> \_uploadImageToFirebase\(File imageFile\) async \{
+try \{
+final storageRef \= firebase\_storage\.FirebaseStorage\.instance
+\.ref\(\)
+\.child\('profile\_pictures'\)
+\.child\('</span>{userController.userId.value}.jpg');
       final uploadTask = storageRef.putFile(imageFile);
       final snapshot = await uploadTask.whenComplete(() {});
       _selectedImageUrl = await snapshot.ref.getDownloadURL();
     } catch (e) {
-      print("Error uploading image to Firebase: $e");
-      _showDialog("Failed to upload profile picture");
+      print("Error mengunggah gambar ke Firebase: $e");
+      _showDialog("Gagal mengunggah gambar profil");
     }
   }
 
+  /// Membuka pemilih gambar untuk memilih gambar profil baru
+  ///
+  /// Membuka pemilih gambar dan memungkinkan pengguna untuk memilih gambar
+  /// dari galeri perangkat mereka. Jika gambar dipilih, perbarui file gambar
+  /// dan URL gambar yang dipilih, dan picu pengunggahan gambar.
   Future<void> _pickImage() async {
     final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
@@ -79,6 +101,10 @@ class _EditProfilePageState extends State<EditProfilePage> {
     }
   }
 
+  /// Menampilkan dialog kesalahan dengan pesan yang disediakan
+  ///
+  /// Membuat dialog peringatan sederhana untuk menampilkan pesan kesalahan
+  /// kepada pengguna.
   void _showDialog(String message) {
     showDialog(
       context: context,
@@ -87,141 +113,4 @@ class _EditProfilePageState extends State<EditProfilePage> {
           content: Text(message),
           actions: [
             TextButton(
-              child: Text("OK"),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void _cancelImageChange() {
-    setState(() {
-      _imageFile = null;
-      _selectedImageUrl = userController.profilePictureUrl.value;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Edit Profile',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-        ),
-        centerTitle: true,
-        leading: IconButton(
-          icon: Icon(
-            Icons.arrow_back_ios,
-            color: Colors.black,
-            size: 20,
-          ),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        actions: [
-          IconButton(
-            icon: Text(
-              'Done',
-              style: TextStyle(
-                  color: Colors.orange,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 17),
-            ),
-            onPressed: _updateProfile,
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              CircleAvatar(
-                backgroundImage: _imageFile != null
-                    ? FileImage(_imageFile!)
-                    : _selectedImageUrl != null
-                        ? NetworkImage(_selectedImageUrl!)
-                        : userController.getProfilePicture(),
-                radius: 50,
-              ),
-              TextButton(
-                onPressed: _pickImage,
-                child: Text('Change Profile Picture'),
-              ),
-              if (_imageFile != null)
-                TextButton(
-                  onPressed: _cancelImageChange,
-                  child: Text('Cancel Change'),
-                ),
-              SizedBox(height: 20),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Name',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ),
-              TextFormField(
-                controller: _nameController,
-                decoration: InputDecoration(
-                  hintText: '${userController.userName.value}',
-                  filled: true,
-                  fillColor: Color(0xFFF7F7F9),
-                  contentPadding: EdgeInsets.symmetric(horizontal: 15),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Name cannot be empty';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 20),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Email',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ),
-              TextFormField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  hintText: '${userController.userEmail.value}',
-                  filled: true,
-                  fillColor: Color(0xFFF7F7F9),
-                  contentPadding: EdgeInsets.symmetric(horizontal: 15),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Email cannot be empty';
-                  }
-                  final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
-                  if (!emailRegex.hasMatch(value)) {
-                    return 'Enter a valid email';
-                  }
-                  return null;
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+              child: Text("OK
