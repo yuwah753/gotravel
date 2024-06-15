f92052152a829f2bdeb64959bdd641363b3b2923
way <150601669+yuwah753@users.noreply.github.com>
2024-06-15 21:28:48 +0700

    payment.dart

--- lib/pages/payment.dart
+++ lib/pages/payment.dart
@@ -4,28 +4,32 @@ import 'package:get/get.dart';
 import 'package:gotravel/controllers/user_controller.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
+// Definisi StatefulWidget untuk halaman pembayaran
 class PaymentPage extends StatefulWidget {
   final DateTime startDate;
   final DateTime endDate;
   final Map<String, dynamic> destination;
 
-  PaymentPage(
-      {required this.startDate,
-      required this.endDate,
-      required this.destination});
+  PaymentPage({
+    required this.startDate,
+    required this.endDate,
+    required this.destination,
+  });
 
   @override
   _PaymentPageState createState() => _PaymentPageState();
 }
 
 class _PaymentPageState extends State<PaymentPage> {
-  String? _selectedPaymentMethod;
-  final _firestore = FirebaseFirestore.instance;
-  final UserController userController = Get.find();
-  String? _qrCodeData;
+  String? _selectedPaymentMethod; // Menyimpan metode pembayaran yang dipilih
+  final _firestore = FirebaseFirestore.instance; // Instance Firestore
+  final UserController userController = Get.find(); // Mengambil instance UserController menggunakan GetX
+  String? _qrCodeData; // Menyimpan data QR Code
 
+  // Fungsi untuk mengonfirmasi pembayaran
   Future<void> _confirmPayment() async {
     try {
+      // Menyimpan data booking ke Firestore
       await _firestore.collection('bookings').add({
         'user_id': userController.userId.value,
         'destination': widget.destination,
@@ -35,6 +39,7 @@ class _PaymentPageState extends State<PaymentPage> {
         'qrCodeData': _qrCodeData,
       });
 
+      // Mengupdate data user untuk menambah jumlah perjalanan
       await _firestore
           .collection('users')
           .doc(userController.userId.value)
@@ -42,18 +47,22 @@ class _PaymentPageState extends State<PaymentPage> {
         'travel_trips': FieldValue.increment(1),
       });
 
+      // Menampilkan pesan sukses
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Payment successful!')),
       );
+      // Kembali ke halaman sebelumnya
       Navigator.pop(context);
     } catch (e) {
       print('Error storing data: $e');
+      // Menampilkan pesan error
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Error processing payment')),
       );
     }
   }
 
+  // Fungsi untuk menghasilkan data QR Code
   void _generateQRCode() {
     final bookingData = {
       'user_id': userController.userId.value,
@@ -65,6 +74,7 @@ class _PaymentPageState extends State<PaymentPage> {
     _qrCodeData = bookingData.toString();
   }
 
+  // Fungsi build untuk membangun tampilan halaman
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -76,6 +86,7 @@ class _PaymentPageState extends State<PaymentPage> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
+            // Dropdown untuk memilih metode pembayaran
             DropdownButtonFormField<String>(
               decoration: InputDecoration(
                 labelText: 'Payment Method',
@@ -103,6 +114,7 @@ class _PaymentPageState extends State<PaymentPage> {
               },
             ),
             SizedBox(height: 20),
+            // Formulir untuk memasukkan informasi kartu kredit
             if (_selectedPaymentMethod == 'Credit Card') ...[
               TextField(
                 decoration: InputDecoration(
@@ -143,6 +155,7 @@ class _PaymentPageState extends State<PaymentPage> {
                 ),
               ),
             ],
+            // Menampilkan QR Code jika metode pembayaran adalah QRIS
             if (_selectedPaymentMethod == 'QRIS' && _qrCodeData != null) ...[
               SizedBox(height: 20),
               Center(
@@ -155,9 +168,9 @@ class _PaymentPageState extends State<PaymentPage> {
               SizedBox(height: 20),
             ],
             Spacer(),
+            // Tombol untuk mengonfirmasi pembayaran
             ElevatedButton(
-              onPressed:
-                  _selectedPaymentMethod != null ? _confirmPayment : null,
+              onPressed: _selectedPaymentMethod != null ? _confirmPayment : null,
               child: Text(
                 'Confirm Payment',
                 style: TextStyle(fontFamily: 'ABeeZee'),
