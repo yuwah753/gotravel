30bc4a33ba7193f12a686bcc1e309376297f5a39
belammy <122104708+belammy@users.noreply.github.com>
2024-06-15 22:50:49 +0700

    Update login.dart

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -12,59 +12,62 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final _formKey = GlobalKey<FormState>();
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
-  bool _isPasswordVisible = false;
-  final _auth = FirebaseAuth.instance;
-  final _firestore = FirebaseFirestore.instance;
+  final _formKey = GlobalKey<FormState>(); // Key untuk form validasi
+  final _emailController = TextEditingController(); // Controller untuk text field email
+  final _passwordController = TextEditingController(); // Controller untuk text field password
+  bool _isPasswordVisible = false; // Status visibility untuk password
+  final _auth = FirebaseAuth.instance; // Instance Firebase Authentication
+  final _firestore = FirebaseFirestore.instance; // Instance Firestore
 
-  final UserController userController = Get.put(UserController());
+  final UserController userController = Get.put(UserController()); // Inisiasi UserController
 
   Future<void> _login() async {
-    if (_formKey.currentState!.validate()) {
+    // Fungsi untuk menangani proses login
+    if (_formKey.currentState!.validate()) { // Validasi form
       try {
         UserCredential userCredential = await _auth.signInWithEmailAndPassword(
           email: _emailController.text,
           password: _passwordController.text,
         );
 
-        final userDoc = await _firestore
+        final userDoc = await _firestore // Ambil data user dari firestore berdasarkan UID
             .collection('users')
             .doc(userCredential.user!.uid)
             .get();
 
         if (userDoc.exists) {
-          userController.setUser(
+          userController.setUser( // Set data user ke UserController
             id: userCredential.user!.uid,
             email: _emailController.text,
             name: userDoc.data()!['name'],
             profilePicUrl: userDoc.data()!['profile_picture'],
           );
           print("User ID: " + userCredential.user!.uid);
-          Navigator.pushReplacement(
+          Navigator.pushReplacement( // Navigasi ke halaman Home dan hapus halaman login dari stack
             context,
             MaterialPageRoute(builder: (context) => HomeScreen()),
           );
         } else {
-          _showDialog('User not found.');
+          _showDialog('User tidak ditemukan.');
         }
       } on FirebaseAuthException catch (e) {
+        // Menangani error dari Firebase Authentication
         if (e.code == 'user-not-found') {
-          _showDialog('No user found for that email.');
+          _showDialog('Email tersebut tidak terdaftar.');
         } else if (e.code == 'wrong-password') {
-          _showDialog('Wrong password provided for that user.');
+          _showDialog('Password yang dimasukkan salah.');
         } else {
-          _showDialog('An error occurred. Please try again.');
+          _showDialog('Terjadi kesalahan. Silahkan coba lagi.');
         }
       } catch (e) {
         print(e);
-        _showDialog('An error occurred. Please try again.');
+        _showDialog('Terjadi kesalahan. Silahkan coba lagi.');
       }
     }
   }
 
   void _showDialog(String message) {
+    // Fungsi untuk menampilkan dialog
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -86,9 +89,11 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // Menghilangkan resize pada bottom inset (keyboard)
       resizeToAvoidBottomInset: false,
       body: Center(
         child: SingleChildScrollView(
+          // Agar konten bisa scroll jika keyboard muncul
           padding: const EdgeInsets.all(20.0),
           child: Container(
             padding: const EdgeInsets.all(20.0),
@@ -99,136 +104,17 @@ class _LoginPageState extends State<LoginPage> {
             child: Form(
               key: _formKey,
               child: Column(
-                mainAxisSize: MainAxisSize.min,
+                mainAxisSize: MainAxisSize.min, // Batasi ukuran minimum kolom
                 children: [
                   SizedBox(height: 20),
                   Text(
-                    'Sign in now',
+                    'Masuk',
                     style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'ABeeZee'),
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'ABeeZee'),
                   ),
                   SizedBox(height: 10),
-                  Text('Please sign in to continue our app',
-                      style:
-                          TextStyle(color: Colors.grey, fontFamily: 'ABeeZee')),
-                  SizedBox(height: 20),
-                  TextFormField(
-                    controller: _emailController,
-                    decoration: InputDecoration(
-                      hintText: 'Email',
-                      filled: true,
-                      fillColor: Color(0xFFF7F7F9),
-                      contentPadding: EdgeInsets.symmetric(horizontal: 15),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide.none,
-                      ),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Email cannot be empty';
-                      }
-                      final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
-                      if (!emailRegex.hasMatch(value)) {
-                        return 'Enter a valid email';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 20),
-                  TextFormField(
-                    controller: _passwordController,
-                    obscureText: !_isPasswordVisible,
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                      filled: true,
-                      fillColor: Color(0xFFF7F7F9),
-                      contentPadding: EdgeInsets.symmetric(horizontal: 15),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide.none,
-                      ),
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                          _isPasswordVisible
-                              ? Icons.visibility
-                              : Icons.visibility_off,
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            _isPasswordVisible = !_isPasswordVisible;
-                          });
-                        },
-                      ),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Password cannot be empty';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 10),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Forget Password?',
-                        style: TextStyle(
-                            color: Colors.orange, fontFamily: 'ABeeZee'),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  TextButton(
-                    onPressed: _login,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue,
-                      padding: EdgeInsets.symmetric(vertical: 15),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Sign In',
-                        style: TextStyle(
-                            color: Colors.white, fontFamily: 'ABeeZee'),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Don't have an account?",
-                          style: TextStyle(
-                              color: Colors.grey, fontFamily: 'ABeeZee')),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => SignUpPage()),
-                          );
-                        },
-                        child: Text(
-                          'Sign up',
-                          style: TextStyle(
-                              color: Colors.orange, fontFamily: 'ABeeZee'),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+                  Text(
+                    'Silahkan masuk untuk melanjutkan',
+                    style: TextStyle(color: Colors.grey, fontFamily: 'ABeeZee'),
