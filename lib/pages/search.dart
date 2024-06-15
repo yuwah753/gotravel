ebff403c4258e702191dfbc5fa4ded3842e4c0e7
Hadziq Umar Azka Mirza <122033699+Hadziq224@users.noreply.github.com>
2024-06-15 22:40:22 +0700

    search.dart

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:gotravel/controllers/user_controller.dart';
+import 'package:get/get.dart'; // Untuk dependency management
+import 'package:gotravel/controllers/user_controller.dart'; // Untuk mengakses user controller
 import 'package:gotravel/pages/detail.dart';
 import 'package:gotravel/pages/home.dart';
 import 'package:gotravel/pages/notif.dart';
 import 'package:gotravel/pages/profile.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart'; // Untuk menampilkan rating bar
+import 'package:cloud_firestore/cloud_firestore.dart'; // Untuk akses ke firestore
 
 class SearchScreen extends StatefulWidget {
   @override
@@ -14,55 +14,56 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  final UserController userController = Get.find();
-  List destinations = [];
-  List filteredDestinations = [];
-  TextEditingController searchController = TextEditingController();
-  final _firestore = FirebaseFirestore.instance;
+  final UserController userController = Get.find(); // Mendapatkan user controller
+  List destinations = []; // Daftar destinasi wisata
+  List filteredDestinations = []; // Daftar destinasi wisata yang difilter
+  TextEditingController searchController = TextEditingController(); // Controller untuk text field pencarian
+  final _firestore = FirebaseFirestore.instance; // Instance Firestore
 
   @override
   void initState() {
     super.initState();
-    fetchDestinations();
+    fetchDestinations(); // Memanggil fungsi untuk mengambil data destinasi
     searchController.addListener(() {
-      filterSearchResults(searchController.text);
+      filterSearchResults(searchController.text); // Menangani perubahan text field pencarian
     });
   }
 
   Future fetchDestinations() async {
     try {
       final QuerySnapshot snapshot =
-          await _firestore.collection('destinations').get();
+          await _firestore.collection('destinations').get(); // Mengambil data dari koleksi 'destinations'
       final List<QueryDocumentSnapshot> documents = snapshot.docs;
       setState(() {
-        destinations = documents.map((doc) => doc.data()).toList();
-        filteredDestinations = destinations;
+        destinations = documents.map((doc) => doc.data()).toList(); // Memproses data menjadi list
+        filteredDestinations = destinations; // Set data awal yang difilter sama dengan semua destinasi
       });
     } catch (e) {
-      print("Error fetching destinations: $e");
+      print("Error fetching destinations: $e"); // Menampilkan pesan error jika terjadi kesalahan
     }
   }
 
   void filterSearchResults(String query) {
-    List dummySearchList = [];
-    dummySearchList.addAll(destinations);
+    List dummySearchList = []; // List temporary untuk proses filtering
+    dummySearchList.addAll(destinations); // Menyalin semua data destinasi ke list temporary
     if (query.isNotEmpty) {
       List dummyListData = [];
       dummySearchList.forEach((item) {
         if (item['destination_name']
-            .toString()
-            .toLowerCase()
-            .contains(query.toLowerCase())) {
+                .toString()
+                .toLowerCase()
+                .contains(query.toLowerCase())) {
+          // Mencari destinasi yang sesuai dengan keyword pencarian (case-insensitive)
           dummyListData.add(item);
         }
       });
       setState(() {
-        filteredDestinations = dummyListData;
+        filteredDestinations = dummyListData; // Update data destinasi yang difilter
       });
       return;
     } else {
       setState(() {
-        filteredDestinations = destinations;
+        filteredDestinations = destinations; // Reset data destinasi yang difilter jika text field kosong
       });
     }
   }
@@ -78,20 +79,20 @@ class _SearchScreenState extends State<SearchScreen> {
             size: 20,
           ),
           onPressed: () {
-            Navigator.pop(context);
+            Navigator.pop(context); // Kembali ke halaman sebelumnya
           },
         ),
         title: Text(
-          'Search',
+          'Pencarian',
           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
         ),
         centerTitle: true,
         actions: [
           TextButton(
             onPressed: () {
-              searchController.clear();
+              searchController.clear(); // Membersihkan text field pencarian
             },
-            child: Text('Cancel', style: TextStyle(color: Colors.blue)),
+            child: Text('Batal', style: TextStyle(color: Colors.blue)),
           ),
         ],
       ),
@@ -100,232 +101,14 @@ class _SearchScreenState extends State<SearchScreen> {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
-              controller: searchController,
+              controller: searchController, // Mengaitkan text field dengan controller
               decoration: InputDecoration(
-                prefixIcon: Icon(Icons.search),
-                suffixIcon: Icon(Icons.mic_none),
-                hintText: 'Search Places',
+                prefixIcon: Icon(Icons.search), // Menampilkan icon pencarian
+                suffixIcon: Icon(Icons.mic_none), // Menampilkan icon microphone (non-fungsional)
+                hintText: 'Cari Tempat', // Text hint untuk text field
                 hintStyle: TextStyle(
                   fontFamily: 'ABeeZee',
                 ),
-                border: InputBorder.none,
-                filled: true,
-                fillColor: Colors.grey[200],
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: BorderSide.none,
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: BorderSide.none,
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: MediaQuery.of(context).size.width < 600 ? 2 : 4,
-                crossAxisSpacing: 3.0,
-                mainAxisSpacing: 3.0,
-                childAspectRatio: 0.72,
-              ),
-              itemCount: filteredDestinations.length,
-              itemBuilder: (context, index) {
-                var destination = filteredDestinations[index];
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            DetailPage(destination: destination),
-                      ),
-                    );
-                  },
-                  child: Container(
-                    child: Card(
-                      color: Colors.transparent,
-                      elevation: 0,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0),
-                        side: BorderSide.none,
-                      ),
-                      child: Stack(
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(8.0),
-                            child: Image.network(
-                              destination['image'],
-                              width: double.infinity,
-                              height: 124,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          Positioned(
-                            top: 8,
-                            right: 8,
-                            child: Opacity(
-                              opacity: 0.40,
-                              child: Container(
-                                width: 30,
-                                height: 30,
-                                decoration: ShapeDecoration(
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(30),
-                                  ),
-                                  color: Color(0xFF1B1E28),
-                                ),
-                                child: Icon(
-                                  Icons.favorite_border_outlined,
-                                  color: Colors.white,
-                                  size: 18,
-                                ),
-                              ),
-                            ),
-                          ),
-                          Positioned(
-                            left: 8,
-                            bottom: 8,
-                            right: 8,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  destination['destination_name'],
-                                  style: TextStyle(fontFamily: 'ABeeZee'),
-                                ),
-                                SizedBox(height: 4.0),
-                                Row(
-                                  children: [
-                                    Icon(Icons.location_on_outlined,
-                                        color: Colors.grey, size: 15),
-                                    SizedBox(width: 3),
-                                    Text(destination['city'],
-                                        style: TextStyle(
-                                            fontFamily: 'ABeeZee',
-                                            color: Colors.grey)),
-                                  ],
-                                ),
-                                SizedBox(height: 5),
-                                Row(
-                                  children: [
-                                    RatingBarIndicator(
-                                      rating:
-                                          double.parse(destination['rating']),
-                                      itemBuilder: (context, index) => Icon(
-                                        Icons.star,
-                                        color: Colors.amber,
-                                      ),
-                                      itemCount: 5,
-                                      itemSize: 20.0,
-                                      direction: Axis.horizontal,
-                                    ),
-                                    SizedBox(width: 5.0),
-                                    Text(
-                                      destination['rating'],
-                                      style: TextStyle(color: Colors.grey),
-                                    ),
-                                  ],
-                                ),
-                                SizedBox(height: 7),
-                                Text.rich(
-                                  TextSpan(
-                                    children: [
-                                      TextSpan(
-                                        text: destination['price'] + 'K',
-                                        style: TextStyle(
-                                          color: Colors.lightBlue[300],
-                                          fontSize: 13,
-                                          fontFamily: 'ABeeZee',
-                                          fontWeight: FontWeight.w400,
-                                        ),
-                                      ),
-                                      TextSpan(
-                                        text: '/Person',
-                                        style: TextStyle(
-                                          color: Color(0xFF7C838D),
-                                          fontSize: 13,
-                                          fontFamily: 'ABeeZee',
-                                          fontWeight: FontWeight.w400,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
-          )
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        elevation: 0.0,
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(
-            icon: Padding(
-              padding: EdgeInsets.symmetric(),
-              child: Image.asset('images/plane.png', height: 24),
-            ),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Padding(
-              padding: EdgeInsets.symmetric(vertical: 10.0),
-              child: Image.asset('images/search-blue.png', height: 28),
-            ),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Padding(
-              padding: EdgeInsets.symmetric(),
-              child: Image.asset('images/notif.png', height: 24),
-            ),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Padding(
-              padding: EdgeInsets.symmetric(),
-              child: Image.asset('images/person.png', height: 24),
-            ),
-            label: '',
-          ),
-        ],
-        onTap: (index) {
-          switch (index) {
-            case 0:
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HomeScreen()),
-              );
-              break;
-            case 1:
-              break;
-            case 2:
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => NotificationScreen()),
-              );
-              break;
-            case 3:
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ProfileScreen()),
-              );
-              break;
-          }
-        },
-      ),
-    );
-  }
-}
+                border: InputBorder.none, // Menghilangkan border default
+                filled: true, // Mengisi background text field
+                fillColor: Colors.grey
